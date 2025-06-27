d116e563f7f2179166e554d0c42770ae61eec508
Nguyen Thanh Trieu <trieukon1011@gmail.com>
2025-06-28 04:16:31 +0700

    add admin site

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || '*',
+  origin:  '*',
   credentials: true
 }));
 app.use(express.json());
